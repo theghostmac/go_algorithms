ed3511ef300f143dc414596a9271d4a9e147c28f
MacBobby Chibuzor <theghostmac@gmail.com>
2022-04-02 23:38:04 +0100

    search tree

--- re_binary_tree.go
+++ re_binary_tree.go
@@ -1,8 +1,15 @@
 package main
 
+import "sync"
+
 type Node struct {
 	key       int
 	value     int
 	leftNode  *Node
 	rightNode *Node
+}
+
+type BinarySearchTree struct {
+	lock     sync.RWMutex
+	rootNode *Node
 }
